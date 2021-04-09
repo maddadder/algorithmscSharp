4ed36ade07ff36162b3298ca9215a852e1a90fbd
clee <clee@tuta.io>
2021-04-09 01:31:16 +0000

    add TextInputHandler

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace algorithmscSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "THE OFFICE party will be in Dec. this year.  the ExacT date will be sET by mid Nov.  hope to see you ALL there!  tickets are $15.00 to help cover the cost of Catering.";
+            string output = TextInputHandler.formatInput(input);
+            Console.WriteLine(output);
+        }
+    }
+}
---
+++ TextInputHandler.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace algorithmscSharp
+{
+    class TextInputHandler
+    {
+        public static string formatInput(string input)
+        {
+            string[] bannedUpper = { "Dec." };
+            string[] bannedSpace = { "$15." };
+            char[] punctuation = ".!".ToCharArray();
+            StringBuilder sb = new StringBuilder();
+            string[] words = input.Split(" ");
+            for(var i = 0;i<words.Length;i++)
+            {
+                string word = words[i];
+                if(string.IsNullOrEmpty(word))
+                    continue;
+                string priorWord = "";
+                string followingWord = "";
+               
+                int j = i;
+                while(j > 0 && string.IsNullOrEmpty(priorWord))
+                {
+                    priorWord = words[j-1];
+                    j--;
+                }
+                j = i;
+                while(j+1 < words.Length && string.IsNullOrEmpty(followingWord))
+                {
+                    followingWord = words[j+1];
+                    j++;
+                }
+                
+                if(i == 0 || punctuation.Contains(priorWord[priorWord.Length - 1]))
+                {
+                    //is first word
+                    if(!bannedUpper.Contains(priorWord))
+                    {
+                        word = word.Substring(0,1).ToUpper() + word.Substring(1).ToLower();
+                    }
+                }
+                else
+                {
+                    if(char.IsUpper(word[0]) && word.Substring(1).ToLower() == word.Substring(1))
+                    {
+                        word = word.Substring(0,1).ToUpper() + word.Substring(1).ToLower();
+                    }
+                    else
+                    {
+                        word = word.ToLower();
+                    }
+                }
+                if(bannedSpace.Contains(word))
+                {
+                    sb.Append(word);
+                }
+                else
+                {
+                    sb.Append(word + " ");
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
