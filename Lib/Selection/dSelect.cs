11f08790d802a344955d75947e4eda4e6512c8f3
Charlie Lee <clee@tuta.io>
2022-12-15 20:09:00 -0800

    convert to using generics

--- Lib/Selection/dSelect.cs
+++ Lib/Selection/dSelect.cs
@@ -9,19 +9,19 @@ namespace Lib.Selection
 {
     public static class dSelect
     {
-        public static int FindNthSmallestNumber(this IList<int> list, int Nth) 
+        public static T FindNthSmallestNumber<T>(this IList<T> list, int Nth) where T : IComparable
         {
             int start = 0;
             int end = list.Count;
             return FindNthSmallestNumber(list, Nth, start, end);
         }
-        public static int MediumOfMediums(this IList<int> list)
+        public static T MediumOfMediums<T>(this IList<T> list) where T : IComparable
         {
             var n = list.Count;
             if(list.Count == 1)
                 return list[0];
             var chunkSize = (int)Math.Ceiling((double)n/5);
-            var C = new int[chunkSize];
+            var C = new T[chunkSize];
             var Ci = 0;
             var chunks = ListExtensions.SplitByChunks(list,5);
             foreach(var chunk in chunks)
@@ -33,18 +33,21 @@ namespace Lib.Selection
             }
             return MediumOfMediums(C);
         }
-        public static int ChoosePivot(this IList<int> list, int start, int end)
+        public static T ChoosePivot<T>(this IList<T> list, int start, int end) where T : IComparable
         {
             var tmp = list.Skip(start).Take(end-start).ToList();
             var median = MediumOfMediums(tmp);
             return median;
         }
         
-        public static int FindNthSmallestNumber(this IList<int> list, int Nth, int start, int end)
+        public static T FindNthSmallestNumber<T>(this IList<T> list, int Nth, int start, int end) where T : IComparable
         {
             if (start >= end)
                 return list[0];
-            var pivotLocation = ChoosePivot(list, start, end);
+            var pivot = ChoosePivot(list, start, end);
+            
+            //this requires the list type is an int
+            var pivotLocation = Convert.ToInt32(pivot);
             var pivotIndex = QuickSortClass.Partition(list,pivotLocation,start,end);
             if(pivotIndex == Nth)
             {
