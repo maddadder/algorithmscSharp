d0ddb8b3050dce614711884366808d59dd9859ff
Charlie Lee <clee@tuta.io>
2023-01-21 09:43:30 -0800

    refactor

--- Test/DynamicProgramming/KnapsackTest.cs
+++ Test/DynamicProgramming/KnapsackTest.cs
@@ -14,7 +14,7 @@ namespace Test.DynamicProgramming
         public void Knapsack_Test(){
             var jobs = new List<Job>();
             Random rand = new Random();
-            var Budget = 10000;
+            var Budget = 10;
             int cost = Budget*2;
             for(var i = 0;i<Budget;i++){
                 var val = rand.Next(1,Budget);
@@ -38,10 +38,10 @@ namespace Test.DynamicProgramming
             Debug.WriteLine($"The total value (in length) of the items in the knapsack is {value}.");
             Debug.WriteLine($"The bag can only carry {cost}");
         }
-        public int knapsack(Job[] input, int Capacity)
+        public int knapsack(Job[] S, int Capacity)
         {
             // subproblem solutions
-            int[, ] matrix = new int[input.Length+1, Capacity+1];
+            int[, ] matrix = new int[S.Length+1, Capacity+1];
 
             // base case
             for(var c = 0;c<=Capacity;c++)
@@ -50,19 +50,29 @@ namespace Test.DynamicProgramming
             }
 
             // systematically solve all subproblems
-            for(var i = 1;i<=input.Length;i++) {
+            for(var i = 1;i<=S.Length;i++) {
                 for(var c = 0;c<=Capacity;c++) {
                     // use recurrence from Corollary 16.5
-                    if (input[i-1].Cost() > c) {
-                        matrix[i, c] = matrix[i-1, c];
+                    var case1 = matrix[i-1, c];
+                    var Si = S[i-1].Cost();
+                    var Vi = S[i-1].Value();
+                    if (Si > c) {
+                        // where case1 is an optimal solution for the first i-1 items with 
+                        // knapsack capacity = Capacity
+                        matrix[i, c] = case1;
                     } else {
-                        var case1 = matrix[i-1, c];
-                        var case2 = matrix[i-1, c-input[i-1].Cost()] + input[i-1].Value();
-                        matrix[i,c] = Math.Max(case1, case2);
+                        // where case2 is an optimal solution for the first i-1 items with
+                        // knapsack capacity = Capacity - Si, supplemented with the last item Vi
+                        var case2 = matrix[i-1, c-Si] + Vi;
+                        if(case1.CompareTo(case2) > 0) {
+                            matrix[i,c] = case1;
+                         } else { 
+                            matrix[i,c] = case2;
+                         }
                     }
                 }
             }
-            return matrix[input.Length, Capacity]; //solution to largest subproblem
+            return matrix[S.Length, Capacity]; //solution to largest subproblem
         }
     }
 }
