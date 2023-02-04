547440901bfe09803a16ac559f19e5e1f581e200
Charlie Lee <clee@tuta.io>
2023-02-04 13:36:02 -0800

    remove old graphs

--- Test/Graphs/TestBellmanFordShortestPathDatasets.cs
+++ Test/Graphs/TestBellmanFordShortestPathDatasets.cs
@@ -1,5 +1,4 @@
 using System.Diagnostics;
-using Lib.Graphs.v2;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Graphs
@@ -13,171 +12,98 @@ namespace Graphs
         {
             string sourceFile = "../../../MST1.txt";
             string[] lines = System.IO.File.ReadAllLines(sourceFile);
-            MathGraph<int> graph = new MathGraph<int>();
-            var bellmanDistA = MathGraph<int>.manageBellmanFord(graph, lines);
-            var bellmanDistASum = bellmanDistA.Sum(x => x.Value);
-            var bellmanDistB = Lib.Graphs.v2.Graph.manageBellmanFord(lines);
-            var bellmanDistBSum = bellmanDistB.Sum(x => x);
-            // Assert
-            Assert.AreEqual(bellmanDistASum, bellmanDistBSum);
 
-            Lib.Graphs.MathGraph<int> graphv1 = new Lib.Graphs.MathGraph<int>();
-            Lib.Graphs.MathGraph<int>.LoadGraph(graphv1, lines, isUndirectedGraph: false);
-            var DijkstraDistA = graphv1.Dijkstra(1);
-            var DijkstraDistSum = DijkstraDistA.Sum(x => x.Value);
-            Assert.AreEqual(bellmanDistASum, DijkstraDistSum);
-
-            var bellmanDistC = graphv1.BellmanFord(1);
-            var bellmanDistCSum = bellmanDistC.Sum(x => x.Value);
-            Assert.AreEqual(bellmanDistASum, bellmanDistCSum);
+            Lib.Graphs.MathGraph<int> graph = new Lib.Graphs.MathGraph<int>();
+            Lib.Graphs.MathGraph<int>.LoadGraph(graph, lines, isUndirectedGraph: false);
+            var bellmanDist = graph.BellmanFord(1);
+            var bellmanDistSum = bellmanDist.Sum(x => x.Value);
+            Assert.AreEqual(6, bellmanDistSum);
         }
         [TestMethod]
         public void TestIn3()
         {
             string sourceFile = "../../../MST3.txt";
             string[] lines = System.IO.File.ReadAllLines(sourceFile);
-            MathGraph<int> graph = new MathGraph<int>();
-            var bellmanDistA = MathGraph<int>.manageBellmanFord(graph, lines);
-            var bellmanDistASum = bellmanDistA.Sum(x => x.Value);
-            var bellmanDistB = Lib.Graphs.v2.Graph.manageBellmanFord(lines);
-            var bellmanDistBSum = bellmanDistB.Sum(x => x);
-            // Assert
-            Assert.AreEqual(bellmanDistASum, bellmanDistBSum);
-
-            Lib.Graphs.MathGraph<int> graphv1 = new Lib.Graphs.MathGraph<int>();
-            Lib.Graphs.MathGraph<int>.LoadGraph(graphv1, lines, isUndirectedGraph: false);
-            var DijkstraDistA = graphv1.Dijkstra(1);
-            var DijkstraDistSum = DijkstraDistA.Sum(x => x.Value);
-            Assert.AreEqual(bellmanDistASum, DijkstraDistSum);
-
-            var bellmanDistC = graphv1.BellmanFord(1);
-            var bellmanDistCSum = bellmanDistC.Sum(x => x.Value);
-            Assert.AreEqual(bellmanDistASum, bellmanDistCSum);
+            
+            Lib.Graphs.MathGraph<int> graph = new Lib.Graphs.MathGraph<int>();
+            Lib.Graphs.MathGraph<int>.LoadGraph(graph, lines, isUndirectedGraph: false);
+            var bellmanDist = graph.BellmanFord(1);
+            var bellmanDistSum = bellmanDist.Sum(x => x.Value);
+            Assert.AreEqual(34, bellmanDistSum);
         }
         [TestMethod]
         public void TestIn4()
         {
             string sourceFile = "../../../MST4.txt";
             string[] lines = System.IO.File.ReadAllLines(sourceFile);
-            MathGraph<int> graph = new MathGraph<int>();
-            var bellmanDistA = MathGraph<int>.manageBellmanFord(graph, lines);
-            var bellmanDistASum = bellmanDistA.Sum(x => x.Value);
-            var bellmanDistB = Lib.Graphs.v2.Graph.manageBellmanFord(lines);
-            var bellmanDistBSum = bellmanDistB.Sum(x => x);
-            // Assert
-            Assert.AreEqual(bellmanDistASum, bellmanDistBSum);
-
-            Lib.Graphs.MathGraph<int> graphv1 = new Lib.Graphs.MathGraph<int>();
-            Lib.Graphs.MathGraph<int>.LoadGraph(graphv1, lines, isUndirectedGraph: false);
-            var DijkstraDistA = graphv1.Dijkstra(1);
-            var DijkstraDistSum = DijkstraDistA.Sum(x => x.Value);
-            Assert.AreEqual(bellmanDistASum, DijkstraDistSum);
-
-            var bellmanDistC = graphv1.BellmanFord(1);
-            var bellmanDistCSum = bellmanDistC.Sum(x => x.Value);
-            Assert.AreEqual(bellmanDistASum, bellmanDistCSum);
+            Lib.Graphs.MathGraph<int> graph = new Lib.Graphs.MathGraph<int>();
+            Lib.Graphs.MathGraph<int>.LoadGraph(graph, lines, isUndirectedGraph: false);
+            var bellmanDist = graph.BellmanFord(1);
+            var bellmanDistSum = bellmanDist.Sum(x => x.Value);
+            Assert.AreEqual(6, bellmanDistSum);
         }
         [TestMethod]
         public void TestInBellmanFord1()
         {
             string sourceFile = "../../../BellmanFord1.txt";
             string[] lines = System.IO.File.ReadAllLines(sourceFile);
-            MathGraph<int> graph = new MathGraph<int>();
-            var bellmanDistA = MathGraph<int>.manageBellmanFord(graph, lines);
-            var bellmanDistASum = bellmanDistA.Sum(x => x.Value);
-            var bellmanDistB = Lib.Graphs.v2.Graph.manageBellmanFord(lines);
-            var bellmanDistBSum = bellmanDistB.Sum(x => x);
-            // Assert
-            Assert.AreEqual(bellmanDistASum, bellmanDistBSum);
-
-            Lib.Graphs.MathGraph<int> graphv1 = new Lib.Graphs.MathGraph<int>();
-            Lib.Graphs.MathGraph<int>.LoadGraph(graphv1, lines, isUndirectedGraph: false);
-            var bellmanDistC = graphv1.BellmanFord(1);
-            var bellmanDistCSum = bellmanDistC.Sum(x => x.Value);
-            Assert.AreEqual(bellmanDistASum, bellmanDistCSum);
+            Lib.Graphs.MathGraph<int> graph = new Lib.Graphs.MathGraph<int>();
+            Lib.Graphs.MathGraph<int>.LoadGraph(graph, lines, isUndirectedGraph: false);
+            var bellmanDist = graph.BellmanFord(1);
+            var bellmanDistSum = bellmanDist.Sum(x => x.Value);
+            Assert.AreEqual(5, bellmanDistSum);
         }
         [TestMethod]
         public void TestInBellmanFord2()
         {
             string sourceFile = "../../../BellmanFord2.txt";
             string[] lines = System.IO.File.ReadAllLines(sourceFile);
-            MathGraph<int> graph = new MathGraph<int>();
-            var bellmanDistA = MathGraph<int>.manageBellmanFord(graph, lines);
-            // Assert
-            Assert.AreEqual(null, bellmanDistA);
-
-            Lib.Graphs.MathGraph<int> graphv1 = new Lib.Graphs.MathGraph<int>();
-            Lib.Graphs.MathGraph<int>.LoadGraph(graphv1, lines, isUndirectedGraph: false);
-            var bellmanDistC = graphv1.BellmanFord(1);
-            Assert.AreEqual(null, bellmanDistC);
+            Lib.Graphs.MathGraph<int> graph = new Lib.Graphs.MathGraph<int>();
+            Lib.Graphs.MathGraph<int>.LoadGraph(graph, lines, isUndirectedGraph: false);
+            var bellmanDist = graph.BellmanFord(1);
+            Assert.AreEqual(null, bellmanDist);
         }
         [TestMethod]
         public void TestInBellmanFord3()
         {
             string sourceFile = "../../../BellmanFord3.txt";
             string[] lines = System.IO.File.ReadAllLines(sourceFile);
-            MathGraph<int> graph = new MathGraph<int>();
-            var bellmanDistA = MathGraph<int>.manageBellmanFord(graph, lines);
-            // Assert
-            Assert.AreEqual(null, bellmanDistA);
-
-            Lib.Graphs.MathGraph<int> graphv1 = new Lib.Graphs.MathGraph<int>();
-            Lib.Graphs.MathGraph<int>.LoadGraph(graphv1, lines, isUndirectedGraph: false);
-            var bellmanDistC = graphv1.BellmanFord(1);
-            Assert.AreEqual(null, bellmanDistC);
+            Lib.Graphs.MathGraph<int> graph = new Lib.Graphs.MathGraph<int>();
+            Lib.Graphs.MathGraph<int>.LoadGraph(graph, lines, isUndirectedGraph: false);
+            var bellmanDist = graph.BellmanFord(1);
+            Assert.AreEqual(null, bellmanDist);
         }
         [TestMethod]
         public void TestInBellmanFord4()
         {
             string sourceFile = "../../../BellmanFord4.txt";
             string[] lines = System.IO.File.ReadAllLines(sourceFile);
-            MathGraph<int> graph = new MathGraph<int>();
-            var bellmanDistA = MathGraph<int>.manageBellmanFord(graph, lines);
-            // Assert
-            Assert.AreEqual(null, bellmanDistA);
-
-            Lib.Graphs.MathGraph<int> graphv1 = new Lib.Graphs.MathGraph<int>();
-            Lib.Graphs.MathGraph<int>.LoadGraph(graphv1, lines, isUndirectedGraph: false);
-            var bellmanDistC = graphv1.BellmanFord(1);
-            Assert.AreEqual(null, bellmanDistC);
+            Lib.Graphs.MathGraph<int> graph = new Lib.Graphs.MathGraph<int>();
+            Lib.Graphs.MathGraph<int>.LoadGraph(graph, lines, isUndirectedGraph: false);
+            var bellmanDist = graph.BellmanFord(1);
+            Assert.AreEqual(null, bellmanDist);
         }
         [TestMethod]
         public void TestInBellmanFord5()
         {
             string sourceFile = "../../../BellmanFord5.txt";
             string[] lines = System.IO.File.ReadAllLines(sourceFile);
-            MathGraph<int> graph = new MathGraph<int>();
-            var bellmanDistA = MathGraph<int>.manageBellmanFord(graph, lines);
-            var bellmanDistASum = bellmanDistA.Sum(x => x.Value);
-            var bellmanDistB = Lib.Graphs.v2.Graph.manageBellmanFord(lines);
-            var bellmanDistBSum = bellmanDistB.Sum(x => x);
-            // Assert
-            Assert.AreEqual(bellmanDistASum, bellmanDistBSum);
-
-            Lib.Graphs.MathGraph<int> graphv1 = new Lib.Graphs.MathGraph<int>();
-            Lib.Graphs.MathGraph<int>.LoadGraph(graphv1, lines, isUndirectedGraph: false);
-            var bellmanDistC = graphv1.BellmanFord(1);
-            var bellmanDistCSum = bellmanDistC.Sum(x => x.Value);
-            Assert.AreEqual(bellmanDistASum, bellmanDistCSum);
+            Lib.Graphs.MathGraph<int> graph = new Lib.Graphs.MathGraph<int>();
+            Lib.Graphs.MathGraph<int>.LoadGraph(graph, lines, isUndirectedGraph: false);
+            var bellmanDist = graph.BellmanFord(1);
+            var bellmanDistSum = bellmanDist.Sum(x => x.Value);
+            Assert.AreEqual(-1344, bellmanDistSum);
         }
         [TestMethod]
         public void TestInBellmanFord6()
         {
             string sourceFile = "../../../BellmanFord6.txt";
             string[] lines = System.IO.File.ReadAllLines(sourceFile);
-            MathGraph<int> graph = new MathGraph<int>();
-            var bellmanDistA = MathGraph<int>.manageBellmanFord(graph, lines);
-            var bellmanDistASum = bellmanDistA.Sum(x => x.Value);
-            var bellmanDistB = Lib.Graphs.v2.Graph.manageBellmanFord(lines);
-            var bellmanDistBSum = bellmanDistB.Sum(x => x);
-            // Assert
-            Assert.AreEqual(bellmanDistASum, bellmanDistBSum);
-
-            Lib.Graphs.MathGraph<int> graphv1 = new Lib.Graphs.MathGraph<int>();
-            Lib.Graphs.MathGraph<int>.LoadGraph(graphv1, lines, isUndirectedGraph: false);
-            var bellmanDistC = graphv1.BellmanFord(1);
-            var bellmanDistCSum = bellmanDistC.Sum(x => x.Value);
-            Assert.AreEqual(bellmanDistASum, bellmanDistCSum);
+            Lib.Graphs.MathGraph<int> graph = new Lib.Graphs.MathGraph<int>();
+            Lib.Graphs.MathGraph<int>.LoadGraph(graph, lines, isUndirectedGraph: false);
+            var bellmanDist = graph.BellmanFord(1);
+            var bellmanDistSum = bellmanDist.Sum(x => x.Value);
+            Assert.AreEqual(296435, bellmanDistSum);
         }
     }
 }
