7132660a1757d0fe262b5d1ab5035fab18db14f3
maddadder <clee@tuta.io>
2023-07-27 17:13:57 -0700

    increment version

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 docker-compose build
-docker push neon-registry.b4c5-2917-0fd3-88a5.neoncluster.io/leenet/calculator:1.11.99
+docker push neon-registry.b4c5-2917-0fd3-88a5.neoncluster.io/leenet/calculator:1.11.100
 
 #calculator
 helm upgrade calculator-calculator -f ./chart/values.yaml -f ./chart/values.calculator.yaml ./chart --namespace leenet
